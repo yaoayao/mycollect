ae13c4298fe3f153afaa455a389ee9991fd07528
wanchao <825059346@qq.com>
2017-09-26 15:35:52 +0800

    是

--- src/test/java/test/test.java
+++ src/test/java/test/test.java
@@ -1,6 +1,8 @@
 package test;
 
+import com.cc.schema.User;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by chengwanchao on 2016/6/9.
@@ -8,8 +10,12 @@ import org.junit.Test;
 public class test {
 
     @Test
-    public void testOne(){
-        System.out.print("11");
+    public void testSchema(){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
 
+        User user = (User) context.getBean("eric");
+        System.out.println(user.getId());
+        System.out.println(user.getName());
+        System.out.println(user.getAge());
     }
 }
